8b4c79e28aaa24d84b1a1ef4abe24cb538c220e9
Fernando Lopez <fer.ragnar19@gmail.com>
2018-12-19 20:43:04 -0300

    practicas con elixir

--- lib/consumir_api.ex
+++ lib/consumir_api.ex
@@ -11,6 +11,22 @@ defmodule ConsumirApi do
       |> show_message
     end
 
+    def interar do
+      abecedario = for n <- ?a..?z, do: << n :: utf8 >>
+      map2 = %{
+        1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7,
+        21 => 1, 22 => 2, 23 => 3, 24 => 4, 25 => 5, 26 => 6, 27 => 7,
+        11 => 1, 12 => 2, 13 => 3, 14 => 4, 15 => 5, 16 => 6, 17 => 7,
+        61 => 1, 62 => 2, 63 => 3, 64 => 4, 65 => 5, 66 => 6, 67 => 7,
+        51 => 1, 52 => 2, 53 => 3, 54 => 4, 55 => 5, 56 => 6, 57 => 7,
+        41 => 1, 42 => 2, 43 => 3, 44 => 4, 45 => 5, 46 => 6, 47 => 7
+      }
+      sorted_map = Enum.to_list(map2)
+      IO.inspect sorted_map
+      IO.puts "Se muestra abecedario de forma ordenada con un for"
+      IO.puts abecedario
+    end
+
     defp request(url) do
       HTTPotion.get url
     end
